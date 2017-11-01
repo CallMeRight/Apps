f281757be4913727f172d1145d4bbc5aab9a4b03
Feb.2 <cocos.zxj@gmail.com>
2017-11-01 17:55:07 +0800

    #add test data#

---
+++ sysrightinsert.sql
@@ -0,0 +1,8 @@
+--INSERT INTO [SysRight] ([Id],[ModuleId],[RoleId],[Rightflag]) values ('administratorRightManage','RightManage','administrator',1)
+--INSERT INTO [SysRight] ([Id],[ModuleId],[RoleId],[Rightflag]) values ('administratorModuleSetting','ModuleSetting','administrator',1)
+
+
+
+--INSERT INTO [SysRightOperate] ([Id],[RightId],[KeyCode],[IsValid]) values ('administratorModuleSettingCreate','administratorModuleSetting','Create',1)
+--INSERT INTO [SysRightOperate] ([Id],[RightId],[KeyCode],[IsValid]) values ('administratorModuleSettingDelete','administratorModuleSetting','Delete',1)
+--INSERT INTO [SysRightOperate] ([Id],[RightId],[KeyCode],[IsValid]) values ('administratorModuleSettingEdit','administratorModuleSetting','Edit',1)
